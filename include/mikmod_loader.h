ea102246648cbb77b32af6a3f8d115146ce22256
AeFrunsteinLuth <LethandiOOP@tutamail.com>
2025-09-26 01:32:14 +0000

    Update mikmod_loader.h

--- include/mikmod_loader.h
+++ include/mikmod_loader.h
@@ -1,17 +1,20 @@
 #ifndef mikmod_loader_h
 #define mikmod_loader_h
 
+#include <stddef.h>
+#include <stdint.h>
+
 extern const uint8_t haiku_s3m_bin[];
 extern const uint32_t haiku_s3m_bin_size;
 
 typedef struct _MY_MEMREADER {
 	MREADER core;
 	const void *buffer;
-	long len;
-	long pos;
+	size_t len;
+	size_t pos;
 } MY_MEMREADER;
 
-MREADER *new_mikmod_mem_reader(const void *buffer, long len);
+MREADER *new_mikmod_mem_reader(const void *buffer, size_t len);
 void delete_mikmod_mem_reader(MREADER* reader);
 
 #endif
