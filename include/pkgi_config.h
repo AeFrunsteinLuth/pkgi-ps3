d282f8c1f13b8f74671efcd0d395c2c7db37f64e
AeFrunsteinLuth <LethandiOOP@tutamail.com>
2025-09-26 18:52:20 +0000

    Update pkgi_config.h

--- include/pkgi_config.h
+++ include/pkgi_config.h
@@ -2,8 +2,11 @@
 
 #include "pkgi_db.h"
 
-void pkgi_load_config(Config* config, char* update_url, uint32_t update_len);
-void pkgi_save_config(const Config* config, const char* update_url, uint32_t update_len);
+// Load config and return 0 on success, non-zero on failure
+int pkgi_load_config(Config* config, char* update_url, size_t buffer_size);
+
+// Save config and return 0 on success, non-zero on failure
+int pkgi_save_config(const Config* config, const char* update_url, size_t buffer_size);
 
 const char* pkgi_content_tag(ContentType content);
 const char* pkgi_get_user_language();
