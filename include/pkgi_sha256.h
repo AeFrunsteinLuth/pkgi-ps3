c505e693cdac3fe1f6f1db2a94903fa270f8a7ff
AeFrunsteinLuth <LethandiOOP@tutamail.com>
2025-09-26 19:14:47 +0000

    Update pkgi_sha256.h

--- include/pkgi_sha256.h
+++ include/pkgi_sha256.h
@@ -1,7 +1,10 @@
 #pragma once
 
 #include "pkgi_utils.h"
-#include <polarssl/sha1.h>
-#include <polarssl/sha256.h>
 
+// PolarSSL / mbedTLS headers for hashing
+#include <polarssl/sha1.h>   // Only include if SHA1 is used elsewhere
+#include <polarssl/sha256.h> // For SHA-256 hashing functions
+
+// Standard SHA-256 digest size (in bytes)
 #define SHA256_DIGEST_SIZE 32
